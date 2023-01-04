823de4b5261fbf2be7ca7385e29c1779902ce49b
Vanya Voloshyn <good.boy.2k19@gmail.com>
2023-01-04 04:58:45 +0200

    WeatherApp

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,167 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <div id="app" :class="typeof  weather.main != 'undefined' && weather.main.temp > 15 ? 'warm':''">
+    <main>
+      <div class="searchBox">
+        <input type="text" class="searchBar" placeholder="Search..." v-model="query" @keypress="fetchWeather"/>
+      </div>
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location"> {{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date"> {{ dateBuilder() }}</div>
+        </div>
+        <div class="weather-box">
+          <div class="weather">{{ weather.weather[0].main }}</div>
+          <div class="temp"> {{ Math.round(weather.main.temp) }}°c</div>
+        </div>
+      </div>
+    </main>
+  </div>
 
+</template>
+/* eslint-disab
+e */
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      api_key: 'a8d9956ff10457c10b3d9d0356412c9b',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  // ${this.url_base}weather?=${this.query}&units=metric&appid=${this.api_key}
+  // https://api.openweathermap.org/data/2.5/weather?lat=40.712776&lon=-74.005974&appid=a8d9956ff10457c10b3d9d0356412c9b
+  methods: {
+    fetchWeather(e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&lat=0&lon=0&units=metric&appid=${this.api_key}`).then(res => {
+          return res.json()
+        }).then(this.setResults)
+      }
+    },
+    setResults(res) {
+      this.weather = res;
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`
+    }
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  background-image: url('https://images.theconversation.com/files/232705/original/file-20180820-30593-1nxanpj.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+#app.warm {
+  background-image: url("https://img.freepik.com/free-vector/best-scene-nature-landscape-mountain-river-forest-with-sunset-evening-warm-tone-illustration_1150-39401.jpg");
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+
+.searchBar {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.searchBox .searchBar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  /*background: none;*/
+  /*background-color: rgba(255, 255, 255, 0.15);*/
+  border-radius: 0 16px 0 16px;
+  box-shadow: 0 0 16px, rgba(0, 0, 0, 0.25);
+  transition: 0.6s;
+}
+
+.searchBar:focus {
+  box-shadow: 0 0 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 16px 0 16px 0;
+}
+
+.searchBox:focus {
+  box-shadow: 0 0 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 16px 0 16px 0;
+
+}
+
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: white;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  border-radius: 16px;
+  background-color: rgba(255, 255, 255, 0.25);
+  margin: 30px 0;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: white;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+
+
 }
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Test {{text}}</h1>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
